a8ba0b40e69adbdd9c58fde61413913b041ee975
Vijay-kumar-07 <vijaykumarbudatha7@gmail.com>
2022-07-29 18:54:49 +0530

    Initial setup

--- init_setup.sh
+++ init_setup.sh
@@ -1,17 +1,24 @@
 echo [$(date)]: "START"
 echo [$(date)]: "creating environment"
-conda create --prefix ./env python=3.7 -y
+conda create --prefix ./env python=3.8 -y
 echo [$(date)]: "activate environment"
 source activate ./env
+echo [$(date)]: "Create folder and file structure"
+
+for DIR in data_ingestion data_preparation data_validation model model_evaluation model_trainer
+do
+    echo "Creating", "src/NER_"$DIR
+    mkdir -p "src/NER_"$DIR
+    echo "Creating __init__.py inside above folders"
+    touch "src/NER_"$DIR/"__init__.py" "src/NER_"$DIR/$DIR".py"
+    echo "from src.NER_"$DIR"."$DIR" import *" >> "src/NER_"$DIR/"__init__.py"
+done
+
 echo [$(date)]: "install requirements"
 pip install -r requirements.txt
-echo [$(date)]: "export conda environment"
-conda env export > conda.yaml
-echo "# ${PWD}" > README.md
-echo [$(date)]: "first commit"
-git add .
-git commit -m "first commit"
-echo [$(date)]: "END"
+echo [$(date)]: "install pytorch dependency"
+pip install torch -q
+echo [$(date)]: "END" 
 
 # to remove everything -
 # rm -rf env/ .gitignore conda.yaml README.md .git/
